8b19b86dd5cb3392a2838a5d7e82bc108ace4182
Aram Peres <aram.peres@wavy.fm>
2021-05-15 18:12:14 -0400

    Implement failing situation for UUIDs

--- test/tests/002_uuid_read_insert.sh
+++ test/tests/002_uuid_read_insert.sh
@@ -8,3 +8,8 @@ docker-compose exec clickhouse clickhouse-client -q "INSERT INTO default.demo_ta
 docker-compose exec clickhouse clickhouse-client -q "SELECT * FROM default.demo_table ORDER BY id;"
 docker-compose exec pg psql -U postgres -c 'IMPORT FOREIGN SCHEMA "default" FROM SERVER clickhouse_srv INTO public;'
 docker-compose exec pg psql -U postgres -c "SELECT * FROM public.demo_table ORDER BY id;"
+docker-compose exec pg psql -U postgres -c "\d public.demo_table"
+
+# Insert from postgres to clickhouse
+docker-compose exec pg psql -U postgres -c "INSERT INTO public.demo_table (id, data) VALUES (1, '33bb9554-c616-42e6-a9c6-88d3bba4221c')"
+docker-compose exec clickhouse clickhouse-client -q "SELECT * FROM default.demo_table ORDER BY id;"
